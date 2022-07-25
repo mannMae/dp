25a4f3c191b6ea6e81d872e597ef5308d5088295
김재만 <daga4242@gmail.com>
2022-07-26 05:39:03 +0900

    feat: 애니멀타입 생성

--- contracts/MintAnimalToken.sol
+++ contracts/MintAnimalToken.sol
@@ -7,8 +7,15 @@ import "./node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721Enu
 contract MintAnimalToken is ERC721Enumerable {
     constructor() ERC721("name", "symbol") {}
 
+    mapping(uint256 => uint256) public animalTypes;
+
     function mintAnimalToken() public {
         uint256 animalTokenId = totalSupply() +1;
+
+        uint256 animalType = uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender, animalTokenId))) %5+1;
+
+        animalTypes[animalTokenId] = animalType;
+
         _mint(msg.sender, animalTokenId);
     }
 }
