0a3d7903517bc9b961e86f13f3c0f279f3670410
김재만 <daga4242@gmail.com>
2022-07-26 12:35:00 +0900

    feat: SaleAnimalToken

---
+++ contracts/SaleAnimalToken.sol
@@ -0,0 +1,24 @@
+// SPDX - License-Identifier: MINT
+
+pragma solidity ^0.8.0;
+
+import "MintAnimalToken.sol";
+
+contract SaleAnimalToken {
+    MintAnimalToken public mintAnimalTokenAddress;
+
+    constructor (address _mintAnimalTokenAddress){
+        mintAnimalTokenAddress = MintAnimalToken(_mintAnimalTokenAddress);
+    }
+
+    mapping(uint256 => uint256) public animalTokenPrice;
+
+    function setForSaleAnimalToken(uint256 _animalTokenId, uint256 _price) public {
+        address animalToken = mintAnimalTokenAddress.ownerOf(_animalTokenId);
+
+        require(animalTokenOwner == msg.sender, "Caller is not animal token owner.");
+        require(_price > 0, "Price is zero or lower.");
+        require(animalTokenPrices[_animalTokenId] ==0, "This animal token is already on sale.");
+        require(mintAnimalTokenAddress.isApprovedForAll(animalTokenOwner, address(this)), "Animal token owner did not approve token.");
+    }
+}
