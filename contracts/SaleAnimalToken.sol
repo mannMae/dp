f6e74ecc3812a08a7cb8de6900b70ec76075bb1d
김재만 <daga4242@gmail.com>
2022-07-29 18:19:01 +0900

    feat: SaleAnimalToken.sol

--- contracts/SaleAnimalToken.sol
+++ contracts/SaleAnimalToken.sol
@@ -11,14 +11,20 @@ contract SaleAnimalToken {
         mintAnimalTokenAddress = MintAnimalToken(_mintAnimalTokenAddress);
     }
 
-    mapping(uint256 => uint256) public animalTokenPrice;
+    mapping(uint256 => uint256) public animalTokenPrices;
+
+    uint256[] public onSaleAnimalTokenArray;
 
     function setForSaleAnimalToken(uint256 _animalTokenId, uint256 _price) public {
-        address animalToken = mintAnimalTokenAddress.ownerOf(_animalTokenId);
+        address animalTokenOwner = mintAnimalTokenAddress.ownerOf(_animalTokenId);
 
         require(animalTokenOwner == msg.sender, "Caller is not animal token owner.");
         require(_price > 0, "Price is zero or lower.");
-        require(animalTokenPrices[_animalTokenId] ==0, "This animal token is already on sale.");
+        require(animalTokenPrices[_animalTokenId] == 0, "This animal token is already on sale.");
         require(mintAnimalTokenAddress.isApprovedForAll(animalTokenOwner, address(this)), "Animal token owner did not approve token.");
+
+        animalTokenPrices[_animalTokenId] = _price;
+
+        onSaleAnimalTokenArray.push(_animalTokenId);
     }
 }
